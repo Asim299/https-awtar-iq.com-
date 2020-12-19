4accee91261d8466a4cd204400a0cb57e9f4bbb4
Asim299 <75340776+Asim299@users.noreply.github.com>
2020-12-19 23:21:15 +0300

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,60 @@
+if(location.protocol!='https:'){location.protocol='https:';}
+  
+$(`<img style="margin-left: -1px;width:101%; margin-top: 0px;pointer-events:none;margin-bottom: 0px;" src="sico/1608408889425.jpg
+" >
+<a id="jfa44"target="_blank"type="a"rel="nofollow"href=""class="label-primary">1 </a></div></div><center> <a id="asim99" href=""><i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i></a>
+</div></div> <div id="asim1"><a id="asim2"target="_blank"type="a"href="rules.html"style="border-radius: 0 10% 0 0; border-right: 2px solid#a6a6a6;"class="btn minix btn-primary">القوانين</a>
+<a id="asim2"target="_blank"type="a"href=""style="border-radius: 0%; border-right: 1px solid#f5f8fa;"class="btn minix btn-primary">الاشتراكات</a>
+<a id="asim10"target="_blank"type="a"href=""style="border-radius: 0 0 10% 0; border-right: 2px solid#a6a6a6; border-top: 1px solid#f5f8fa"class="btn minix btn-primary">اتصل بنا</a>
+<a id="asim10"target="_blank"type="a"href=""style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">الراديو</a>
+<a id="asim10"target="_blank"type="a"href=" "style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">تطبيق</a>
+<a id="asim10"target="_blank"type="a""style="border-radius: 0 0 0 10%; border-left: 2px solid#a6a6a6; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">ختصارات</a>
+<a id="hack2"target="_blank"type="a"href=""style="border-radius: 0 0 10% 0; border-right: 2px solid#a6a6a6; border-top: 1px solid#f5f8fa"class="btn minix btn-primary">المدونه</a>
+<a id="hack2"target="_blank"type="a"href=""style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">الأبراج</a>
+<a id="hack2"target="_blank"type="a"href=""style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">وظائف</a>
+`).insertBefore(".nav-tabs")
+$(".ae.label.label-primary.fa.fa-gear").text(' ضبط '); 
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="as13" src="" width="70%" style="">`)
+$(`<div><marquee direction="right" id="as43" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3"> &nbsp;1 <font color="#595757" class="jfa99">2</font> 3 <font class="jfa99" color="#727A54">&nbsp;4 &nbsp;</font> 5 <font class="jfa88" color="#ff9900">&nbsp;6 &nbsp;</font> 7 <font class="jfa88" color="#ff00ff">&nbsp; 8 &nbsp;</font> 9 <font class="jfa88" color="#5CAD9A">&nbsp; 10 &nbsp;</font> 11 <font class="jfa88" color="#800000">&nbsp; 12 &nbsp;</font> 13 <font class="jfa88" color="#417570">&nbsp; 14 &nbsp;</font>15 &nbsp; </marquee></div><center>`).insertBefore('div#tlogins .lonline.light.break'); 
+$(`<link rel="stylesheet" href="https://raw.githack.com/hussienail/awtar-iq/main/33.css"></style>`).insertBefore("body")
+$(".dpnl").css('width','310px'); 
+$( "div#l1 button.btn.btn-primary" ).bind( "click", function() { alert( "اهلا وسهلا بكم في شات نجمه الحب سجل معنا عضويه لتحصل على لايكات ولدايه وزخرفه وحفظ بروفايلك وكل عام وانتم بخير سنه سعيده عليكم 2021" ); 
+}); 
+
+$("div#tlogins button.btn.btn-primary")
+    .click(function () {
+        var b = setInterval(function () {
+            var c = $(".pmsgc")
+                .length;
+            0 < c ? ($(` </div><center>
+
+`)
+                .appendTo("#mic"), clearInterval(b)) : console.log(c)
+        }, 1)
+    }), $("div#tlogins button.btn.btn-primary")
+    .click(function () {
+        setTimeout(function () {
+            refreshonline()
+        }, 0)
+    }), setInterval(function () {
+        var a = $("#dpnl");
+        0 < a.length && a.remove()
+    }, 0), $(".dpnl")
+    .append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><center></div><center></div>`), $('#d0')
+    .append(`<label title="صاحب الموقع"href="#"onclick="$('.pnhead').text($(this).attr('title'));
+hl($(this),'primary');
+setTimeout(function()
+	{
+	$('#mic').scrollTop(0);
+}
+,0);
+$('.dpnl').show();
+"data-toggle="tab"data-target="#mic"class="ae fa label label-primary fa-volume-up">&nbsp;</label>`);
+$(`<!--<a class="fl fa fa-image btn ui-corner-all ui-shadow ui-btn ui-btn-inline borderg" style="color:purple;margin:2px;width: 106px;text-align: center;font-size:15px!important;"><span dir="rtl"> (0) </span><span style="font-size: 15px!important;>3 </span></a>-->
+<a href="" target="_blank" style="width:107px; margin-right: 5px;
+margin-left: 2px;text-align: center;float: right;border-radius: 5px;
+padding: 3px;font-family: 'Cairo', sans-serif, Fontawesome;font-size: 13px!important;" class="SLPBeats fab fa-angellist" ASIM * BINEN </a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+AddMsg_=AddMsg;AddMsg=function(wid,data){data.msg=data.msg.replace(/\u102C/g,"");return AddMsg_(wid,data);}
+
+$('.fr.borderg a').text(' 2 ')
