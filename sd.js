9c0af83d4b0871cbf17db86e296b82ae2e962b2f
Asim299 <75340776+Asim299@users.noreply.github.com>
2020-12-24 18:43:32 +0300

    Create sd.js

---
+++ sd.js
@@ -0,0 +1,54 @@
+if(location.protocol!='https:'){location.protocol='https:';}
+  
+$(`<img style="margin-left: -1px;width:101%; margin-top: 0px;pointer-events:none;margin-bottom: 0px;" src="sico/1608824582810.jpg
+" >
+<a id="jfa44"target="_blank"type="a"rel="nofollow"href=""class="label-primary">1 </a></div></div><center> <a id="asim99" href=""><i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i></a>
+</div></div> <div id="asim1"><a id="asim2"target="_blank"type="a"href="rules.html"style="border-radius: 0 10% 0 0; border-right: 2px solid#a6a6a6;"class="btn minix btn-primary">القوانين</a>
+<a id="asim2"target="_blank"type="a"href=""style="border-radius: 0%; border-right: 1px solid#f5f8fa;"class="btn minix btn-primary">الاشتراكات</a>
+<a id="asim10"target="_blank"type="a"href=""style="border-radius: 0 0 10% 0; border-right: 2px solid#a6a6a6; border-top: 1px solid#f5f8fa"class="btn minix btn-primary">اتصل بنا</a>
+<a id="asim10"target="_blank"type="a"href=""style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">الراديو</a>
+<a id="asim10"target="_blank"type="a"href=" "style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">تطبيق</a>
+<a id="asim10"target="_blank"type="a""style="border-radius: 0 0 0 10%; border-left: 2px solid#a6a6a6; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">ختصارات</a>
+<a id="hack2"target="_blank"type="a"href=""style="border-radius: 0 0 10% 0; border-right: 2px solid#a6a6a6; border-top: 1px solid#f5f8fa"class="btn minix btn-primary">المدونه</a>
+<a id="hack2"target="_blank"type="a"href=""style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">الأبراج</a>
+<a id="hack2"target="_blank"type="a"href=""style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">وظائف</a>
+`).insertBefore(".nav-tabs")
+$(`<link rel="stylesheet" href="https://raw.githack.com/Asim299/https-awtar-iq.com-/main/ASIm.css">`).insertBefore("body");
+$(".ae.label.label-primary.fa.fa-gear").text(' ضبط '); 
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="as13" src="" width="70%" style="">`)
+$(`<div><marquee direction="right" id="as43" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3"> &nbsp; أهلا وسهلا بكم في شات <font color="#595757" class="jfa99">نـجـمـه الـحـب</font> ● · ‫أفضل سوبر لهذا الأسبوع · ●  <font class="jfa99" color="#727A54">&nbsp;قريبا &nbsp;</font> ● · ‫أفضل زائر لهذا الأسبوع · ●  <font class="jfa88" color="#ff9900">&nbsp;قريبا &nbsp;</font> ● · ‫أفضل زائرة لهذا الأسبوع · ●  <font class="jfa88" color="#ff00ff">&nbsp; قريبا &nbsp;</font> ● · مبدع الحائط لهذا الأسبوع · ●  <font class="jfa88" color="#5CAD9A">&nbsp; قريبا &nbsp;</font> ● · ‫أفضل تواجد لهذا الأسبوع · ●   <font class="jfa88" color="#800000">&nbsp; قريبا&nbsp;</font> ● · مبدعة الحائط لهذا الأسبوع · ●  <font class="jfa88" color="#417570">&nbsp; قريبا &nbsp;</font>● · مع تمنياتنا للجميع بـ أطيب الاوقات &nbsp; </marquee></div><center>`).insertBefore('div#tlogins .lonline.light.break'); $(`<link rel="stylesheet" href="https://raw.githack.com/hussienail/awtar-iq/main/33.css"></style>`).insertBefore("body")
+$(".dpnl").css('width','310px'); 
+$( "div#l1 button.btn.btn-primary" ).bind( "click", function() { alert( "اهلا وسهلا بكم في شات نجمه الحب سجل معنا عضويه لتحصل على لايكات ولدايه وزخرفه وحفظ بروفايلك وكل عام وانتم بخير سنه سعيده عليكم 2021" ); 
+}); 
+
+$("div#tlogins button.btn.btn-primary")
+    .click(function () {
+        var b = setInterval(function () {
+            var c = $(".pmsgc")
+                .length;
+            0 < c ? ($(` </div><center>
+`)
+                .appendTo("#mic"), clearInterval(b)) : console.log(c)
+        }, 1)
+    }), $("div#tlogins button.btn.btn-primary")
+    .click(function () {
+        setTimeout(function () {
+            refreshonline()
+        }, 0)
+    }), setInterval(function () {
+        var a = $("#dpnl");
+        0 < a.length && a.remove()
+    }, 0), $(".dpnl")
+    .append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><center></div><center></div>`), $('#d0')
+    .append(`<label title="صاحب الموقع"href="#"onclick="$('.pnhead').text($(this).attr('title'));
+hl($(this),'primary');
+setTimeout(function()
+	{
+	$('#mic').scrollTop(0);
+}
+,0);
+$('.dpnl').show();
+"data-toggle="tab"data-target="#mic"class="ae fa label label-primary fa-volume-up">&nbsp;</label>`);
+
+$('.fr.borderg a').text(' بـرمـجـه دوك مـوبـايـل')
